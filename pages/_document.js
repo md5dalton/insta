982b41733c67190163c0593e05f46a081bd8fc8f
md5dalton <md5daltonggs@gmail.com>
2023-01-09 20:36:32 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <link rel="stylesheet" type="text/css" href="/fonts/linearicons_coders/css/linearicons.css" />
+      </Head>
+      <body className="overflow-scroll">
         <Main />
         <NextScript />
       </body>
