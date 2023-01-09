b397a01ecf574e2da543140f0d386f0df87b86d7
md5dalton <md5daltonggs@gmail.com>
2023-01-09 20:37:11 +0200

    Update Button.js

--- components/Button.js
+++ components/Button.js
@@ -1,6 +1,6 @@
 export default ({ children, className, ...props }) => (
     <button 
-        className={`flex items-center border rounded-lg gap-x-4 px-6 py-2 ${className}`}
+        className={`grid place-items-center border rounded-lg gap-x-4 px-6 py-2 ${className}`}
         {...props}
     >
         {children}
