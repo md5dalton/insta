29e26064a033712cb38a7eb6e45dfdfb312ec91c
md5dalton <md5daltonggs@gmail.com>
2023-01-09 20:36:47 +0200

    Update Post.js

--- components/Post.js
+++ components/Post.js
@@ -1,22 +1,52 @@
-import Button from "./Button"
-import Icon from "./icon"
-import Profile from "./Profile"
+import Image from "next/image"
+import { ReactImageCarouselViewer } from "react-image-carousel-viewer"
+import ButtonIcon from "./ButtonIcon"
+import ContextMenu from "./ContextMenu"
+import ImageViewer from "./ImageViewer"
 
-export default ({ owner }) => {
+const Profile = ({ children, username, picture }) => {
+    return (
+        <div className="profile container flex items-center gap-x-4 py-2">
+            <Image
+                className="rounded-full shrink-0"
+                src={picture}
+                alt={username}
+                width={40}
+                height={40}
+                style={{objectFit: "cover", height: 40}}
+            />
+            <div className="details grid gap-4 grow">
+                <div className="name flex items-center gap-x-4">
+                    <div className="username">{username}</div>
+                </div>
+                {children && <div className="other">{children}</div>}
+            </div>
+            <ContextMenu></ContextMenu>
+        </div>
+    )
+}
+
+export default ({ owner, post }) => {
+
+    const images = post.images.map(image => ({
+        src: image
+    }))
+
+    console.log(images)
+
     return (
         <div className="post">
-            <Profile {...owner} >
-                <div className="buttons flex items-center gap-4 flex-wrap">
-                    <Button>
-                        <span>Follow</span>
-                        <Icon name="chevron-down" />
-                    </Button>
-                    <Button>Message</Button>
-                    <Button>
-                        <Icon name="user" />
-                    </Button>
-                </div>
-            </Profile>
+            <Profile {...owner} ></Profile>
+            <div className="images">
+                <ImageViewer images={post.images} />
+            </div>
+            <div className="container icon-buttons grid grid-cols-4">
+                <ButtonIcon name="heart" />
+                <ButtonIcon name="enter-down" />
+                <ButtonIcon name="lock" />
+                <ButtonIcon name="bookmark2" className="justify-self-end" />
+            </div>
+            
         </div>
     )
 }
