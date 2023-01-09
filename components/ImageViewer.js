01a9233bd4c78b1101e87cc2ab05db6e1d8f15a2
md5dalton <md5daltonggs@gmail.com>
2023-01-09 21:45:04 +0200

    Update ImageViewer.js

--- components/ImageViewer.js
+++ components/ImageViewer.js
@@ -1,26 +1,42 @@
-import Image from "next/image";
-import List from "./List";
+import Image from "next/image"
+import List from "./List"
 
-export default ({ images }) => (
-    <div className="image-viewer">
-        <List
-            className="flex overflow-x-scroll"
-            items={images}
-            itemHandler={({ url, width, height }, index) => (
-                <li key={index} className="post-image grid justify-center shrink-0 w-full">
-                    <Image
-                        className="row-span-full col-span-full"
-                        src={url}
-                        width={width}
-                        height={height}
-                        // fill
-                    />
-                    <div className="overlay row-span-full col-span-full bg-[#00000087]">
-                        <div className="controls"></div>
-                        <div className="indicators"></div>
-                    </div>
-                </li>
-            )}
-        />
-    </div>
+const Indicators = ({ items, current }) => (
+    <List
+        className="indicators flex justify-center gap-x-2 p-4"
+        items={items}
+        itemHandler={(item, index) => (
+            <div 
+                key={index} 
+                className={`indicator w-2 h-2 rounded-full border ${current === index ? "bg-white" : ""}`}
+            ></div>
+        )}
+    />
 )
+
+export default ({ images }) => {
+    
+    return (
+        <div className="image-viewer">
+            <List
+                className="flex overflow-x-scroll snap-x snap-mandatory"
+                items={images}
+                itemHandler={({ url, width, height }, index) => (
+                    <li key={index} className="post-image grid justify-center shrink-0 w-full snap-start snap-always">
+                        <Image
+                            className="row-span-full col-span-full"
+                            src={url}
+                            width={width}
+                            height={height}
+                            // fill
+                        />
+                        <div className="overlay row-span-full col-span-full bg-[#00000087] flex flex-col">
+                            <div className="controls grow"></div>
+                            <Indicators items={images} current={index} />
+                        </div>
+                    </li>
+                )}
+            />
+        </div>
+    )
+}
