9094f49653c8023ccac0af82329a592a6b4feb3b
md5dalton <md5daltonggs@gmail.com>
2023-01-11 07:47:45 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,31 +10,40 @@ const posts = [
       username: "iggyiz",
       picture: "/images/iggy/profile.jpg"
     },
-    post: {
-      id: 1,
-      images: [
-        {
-          url: "/images/iggy/image-1.jpg",
-          width: 352,
-          height: 499
-        },
-        {
-          url: "/images/iggy/image-2.jpg",
-          width: 375,
-          height: 469
-        },
-        {
-          url: "/images/iggy/image-3.jpg",
-          width: 434,
-          height: 500
-        },
-        {
-          url: "/images/iggy/image-4.jpg",
-          width: 1024,
-          height: 1280
-        }
-      ]
-    }
+    images: [
+      {
+        url: "/images/iggy/image-1.jpg",
+        width: 352,
+        height: 499
+      },
+      {
+        url: "/images/iggy/image-2.jpg",
+        width: 375,
+        height: 469
+      },
+      {
+        url: "/images/iggy/image-3.jpg",
+        width: 434,
+        height: 500
+      },
+      {
+        url: "/images/iggy/image-4.jpg",
+        width: 1024,
+        height: 1280
+      }
+    ],
+    caption: "hello world",
+    tags: [
+      {
+        id: 1,
+        name: "beatiful people"
+      },
+      {
+        id: 2,
+        name: "curiosity"
+      }
+    ],
+    timestamp: "1 hour ago"
   }
 ]
 
