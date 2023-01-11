1ce2726ce173a0e368c20addbccc49c34d422cb0
md5dalton <md5daltonggs@gmail.com>
2023-01-11 07:47:40 +0200

    Delete Button.js

--- components/Button.js
+++
@@ -1,8 +0,0 @@
-export default ({ children, className, ...props }) => (
-    <button 
-        className={`grid place-items-center border rounded-lg gap-x-4 px-6 py-2 ${className}`}
-        {...props}
-    >
-        {children}
-    </button>
-)
