8e7081ddfa9b5f9aab57b44db923bcf53b1fad98
md5dalton <md5daltonggs@gmail.com>
2023-01-14 18:07:56 +0200

    Update List.js

--- components/List.js
+++ components/List.js
@@ -1,5 +1,8 @@
-export default ({ items, itemHandler, ...props }) => (
+export default ({ items, itemHandler, empty, ...props }) => (
     <ul {...props} role="list">
-        {items.map(itemHandler)}
+        {
+            !items.length ? empty :
+            items.map(itemHandler)
+        }
     </ul>
 )
