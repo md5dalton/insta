ec1128ce7b9636d8bcb933260e5cfb71b4faa414
md5dalton <md5daltonggs@gmail.com>
2023-01-14 18:07:43 +0200

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -1,4 +1,7 @@
+import Link from "next/link";
+
 export default () => (
-    <nav>
+    <nav className="grow">
+        <Link className="font-bold tracking-wider" href="/">LETLAPA</Link>
     </nav>
 )
