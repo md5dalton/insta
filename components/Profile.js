1576be5121f5d7b0f365e422d12504ec74ed8d5e
md5dalton <md5daltonggs@gmail.com>
2023-01-14 18:07:32 +0200

    Update Profile.js

--- components/Profile.js
+++ components/Profile.js
@@ -1,30 +1,25 @@
 import Image from "next/image"
-import Button from "./Button"
-import Icon from "./icon"
+import Link from "next/link"
+import ContextMenu from "./ContextMenu"
 
-export default ({ children, username, picture }) => {
-    return (
-        <div className="profile container flex items-center gap-x-4">
+export default ({ children, username, name, picture }) => (
+    <div className="profile flex items-center gap-x-4 py-2">
+        <Link href={`/user/${username}`}>
             <Image
                 className="rounded-full shrink-0"
                 src={picture}
                 alt={username}
-                width={80}
-                height={80}
-                style={{objectFit: "cover", height: 80}}
+                width={40}
+                height={40}
+                style={{objectFit: "cover", height: 40}}
             />
-            <div className="details grid gap-4">
-                <div className="name flex items-center gap-x-4">
-                    <div className="username">{username}</div>
-                    <div className="context-menu">
-                        <Button>
-                            <Icon name="ellipsis" />
-                        </Button>
-                        <div className="menu"></div>
-                    </div>
-                </div>
-                {children && <div className="other">{children}</div>}
+        </Link>
+        <div className="details grid gap-4 grow">
+            <div className="name flex items-center gap-x-4">
+                <Link href={`/user/${username}`} className="username">{name}</Link>
             </div>
+            {children && <div className="other">{children}</div>}
         </div>
-    )
-}
+        <ContextMenu></ContextMenu>
+    </div>
+)
