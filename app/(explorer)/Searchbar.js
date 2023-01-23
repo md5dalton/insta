8a27fe7367849bf6032314810193d77912ab5e0a
md5dalton <md5daltonggs@gmail.com>
2023-01-23 18:10:02 +0200

    Update Searchbar.js

--- app/(explorer)/Searchbar.js
+++ app/(explorer)/Searchbar.js
@@ -1,57 +1,66 @@
 "use client"
 import Link from "next/link"
-import { useRouter } from "next/navigation"
-import { useState } from "react"
+import { usePathname, useRouter } from "next/navigation"
+import { useRef, useState } from "react"
 import Button from "../../components/elements/Button"
-import ButtonIcon from "../../components/elements/ButtonIcon"
 import Icon from "../../components/elements/Icon"
 
 export default () => {
+
+    const input = useRef(null)
     
     const router = useRouter()
 
-    const [ focused, setFocused ] = useState(false)
-    const [ typing, setTyping ] = useState(false)
-    const [ hovering, setHovering ] = useState(false)
+    const pathname = usePathname()
 
-    const typingHandler = () => setTyping(true)
-    
+    const [ focused, setFocused ] = useState(false)
+
     const focusHandler = () => {
         setFocused(true)
         router.push("/recent")
     }
-    const blurHandler = () => {
-        setFocused(false)
-        setTyping(false)
+    const blurHandler = () =>  setFocused(false)
+
+    const searchHandler = ev => {
+        ev.preventDefault()
+        
+        const term = ev.target.value
+        
+        if (term) router.push(`/search/${term.replaceAll(" ", "+")}`)
+
     }
 
+    const reset = () => input.current.value = ""
+
     return (
-        <div className="container header flex items-center gap-4 py-4">
+        <form className="container header flex items-center gap-4 py-4">
             <Link
                 href="/explore" 
-                className={`${focused || hovering ? "" : "hidden"}`}
-                onMouseEnter={() => setHovering(true)}
-                onMouseLeave={() => setHovering(false)}
+                className={`${pathname === "/explore" ? "hidden" : ""}`}
             >
                 <Icon name="arrow-left" />
             </Link>
             <div className="flex items-center gap-4 grow bg-white/20 h-9 px-4 rounded-xl">
                 <Icon 
                     name="magnifier" 
-                    size={`text-sm font-semibold${focused ? " hidden" : ""}`}
+                    size={`text-sm font-semibold${pathname === "/explore" ? "" : " hidden"}`}
                 />
                 <input
                     onFocus={focusHandler}
-                    onInput={typingHandler}
                     onBlur={blurHandler}
+                    onChange={searchHandler}
+                    ref={input}
                     type="text" 
                     placeholder="Search" 
                     className="grow bg-transparent outline-none"
                 />
-                <Button className={`${typing ? "" : "hidden"}`}>
+                <Button
+                    onClick={() => reset()}
+                    className={`${pathname.includes("/search") ? "" : "hidden"}`}
+                >
                     <Icon name="cross2" size="text-sm font-semibold" />
                 </Button>
             </div>
-        </div>
+        </form>
     )
 }
