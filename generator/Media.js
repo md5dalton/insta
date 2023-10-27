4fafaf2a9b96985b3fb8fcca8748f2d1f72a68d1
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-10-27 14:19:13 +0200

    Create Media.js

---
+++ generator/Media.js
@@ -0,0 +1,21 @@
+
+export default class Media
+{
+
+    path
+
+    constructor (path, stats, metadata) {
+        
+        const { birthtime } = stats
+        const { height, width } = metadata
+        
+        this.path = path
+
+        this.height = height
+        this.width = width
+        this.mktime = birthtime
+        this.portrait = height > width
+
+    }
+
+}
