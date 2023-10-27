88393d602dd12dcfd2d6e1b7a09a1846ff5e991b
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-10-27 19:04:53 +0200

    Create DBManager.js

---
+++ generator/DBManager.js
@@ -0,0 +1,64 @@
+import { PrismaClient } from "@prisma/client"
+import Filemanager from "./Filemanager.js"
+
+export default class DBManager
+{
+    #fm
+    #prisma
+
+    #models = ["rootCollection", "collection", "user", "post", "media"]
+
+    #existing = {}
+
+    constructor (path, ext) {
+
+        this.#fm = new Filemanager(path, ext)
+        this.#prisma = new PrismaClient()
+
+    }
+
+    async create () {
+
+        await this.#fm.createSuperCollections()
+        this.#fm.createModels()
+
+        this.init()
+            .then(async () => await this.#prisma.$disconnect())
+            .catch(async e => {
+                console.log(e)
+                await this.#prisma.$disconnect()
+                process.exit(1)
+            })
+    }
+
+    async getExisting () {
+
+        const existing = {}
+
+        for (const model of this.#models) existing[model] = await this.#prisma[model].findMany({
+            select: {
+                id: true
+            }
+        })
+
+        for (const model in existing) this.#existing[model] = existing[model].map(item => item.id)
+        
+
+    }
+
+    filterModels (model) {
+
+        return this.#fm[model + "s"].filter(({ id }) => !this.#existing[model].includes(id))
+
+    }
+
+    async init () {
+
+        await this.getExisting()
+
+        for (const model of this.#models) await this.#prisma[model].createMany({
+            data: this.filterModels(model)
+        })
+
+    }
+}
