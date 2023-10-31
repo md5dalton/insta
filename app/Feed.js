13fe70819765f865f99c608b45e768e742c351f0
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-10-31 11:22:00 +0200

    Update Feed.js

--- app/Feed.js
+++ app/Feed.js
@@ -1,25 +1,11 @@
 "use client"
-import prisma from "@/utils/prisma"
-import Post from "../components/Post/Post"
+import Post from "@/components/Post/Post"
 import InfiniteList from "./InfiniteList"
-import UList from "./UList"
-import { getPosts } from "@/actions/feed"
 
 export default ({ path }) => {
+
     const mediaHandler = media => <Post {...media} /> 
    
-    // const posts = await getPosts()
-
-    // return (
-    //     <UList
-    //         className="grid gap-8"
-    //         items={posts}
-    //         itemHandler={item => <Post {...item} />}
-    //     />
-    // )
-    // console.log(posts[0])
-    // const mediaHandler = media => <Post {...media} /> 
-    // return <img src={`/api/media/${posts[0].picture}`} />      
     return (
         <InfiniteList
             className="grid gap-8"
