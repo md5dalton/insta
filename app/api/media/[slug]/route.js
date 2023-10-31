b5b0618ce1dc3fdf9a3b798ac313437f8c64bd8e
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-10-31 09:57:48 +0200

    Update route.js

--- app/api/media/[slug]/route.js
+++ app/api/media/[slug]/route.js
@@ -1,6 +1,5 @@
 import prisma from "@/utils/prisma"
 import fs from "fs"
-import path from "path"
 
 export async function GET(request, { params: { slug } }) {
 
@@ -33,33 +32,11 @@ export async function GET(request, { params: { slug } }) {
 
         const imagePath = "a:/media/images" + rootCollection.path + collection.path + user.path + post.path.split("<").shift() + media.path
         
-        if (fs.existsSync(imagePath)) {
-            
-            return new Response(fs.readFileSync(imagePath), {
+        return fs.existsSync(imagePath) ? new Response(fs.readFileSync(imagePath), {
                 headers: {"Content-Type": "image/jpeg"},
                 status: 200
-            })
-
-        } else return new Response('Image not found', { status: 404 })
+        }) : new Response('Image not found', { status: 404 })
 
     } else return new Response('Media not found', { status: 404 })
 
-
-    // response.body = 
-
-    // if (fs.existsSync(imagePath)) {
-        
-    //     const image = fs.readFileSync(imagePath)
-
-    //     res.setHeader("Content-Type", "image/jpeg")
-
-    //     res.status(200).end(image, "binary")
-
-    // } else {
-    //     res.status(404).end("Image not found")
-    // }
-    // return new Response('Hello, Next.js!', {
-    //     status: 200,
-    //     headers: { 'Set-Cookie': `token=${token}` },
-    // })
 }
