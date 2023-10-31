b4c000aaebcca18751009985bc0dd921c7d3701b
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-10-31 08:21:31 +0200

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#050418"
+      },
+      container: {
+        padding: '1rem',
+      },
+    }
+  },
+  plugins: [],
+}
