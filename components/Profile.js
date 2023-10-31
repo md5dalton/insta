a473ef4861b60da6d20c24fad987e5e252f1de2b
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-10-31 08:22:00 +0200

    Update Profile.js

--- components/Profile.js
+++ components/Profile.js
@@ -2,17 +2,17 @@ import Link from "next/link"
 import ContextMenu from "./ContextMenu"
 import ProfilePhoto from "./elements/ProfilePhoto"
 
-export default ({ children, username, name, picture, size = "S", isLink = true }) => (
+export default ({ children, id, name, picture, size = "S", isLink = true }) => (
     <div className="profile flex items-center gap-x-4 py-4">
         <ProfilePhoto
-            url={isLink ? `/user/${username}` : ""}
-            src={picture}
-            alt={username}
+            url={isLink ? `/user/${id}` : ""}
+            src={`/api/media/${picture}`}
+            alt={id}
             size={size}
         />
         <div className="details grid gap-4 grow">
             <div className="name flex items-center gap-x-4">
-                <Link href={`/user/${username}`} className="username font-semibold">{name}</Link>
+                <Link href={`/user/${id}`} className="username font-semibold">{name}</Link>
             </div>
             {children && <div className="other">{children}</div>}
         </div>
