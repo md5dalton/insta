99e58b3c789be6994e77a91b65e6f06a296fe679
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-10-31 09:57:50 +0200

    Update Feed.js

--- app/Feed.js
+++ app/Feed.js
@@ -1,47 +1,31 @@
-// "use client"
+"use client"
 import prisma from "@/utils/prisma"
 import Post from "../components/Post/Post"
 import InfiniteList from "./InfiniteList"
 import UList from "./UList"
+import { getPosts } from "@/actions/feed"
 
-export default async ({ path }) => {
+export default ({ path }) => {
+    const mediaHandler = media => <Post {...media} /> 
+   
+    // const posts = await getPosts()
 
-    const posts = await prisma.post.findMany({
-        take: 11,
-        include: {
-            media: {
-                select: {
-                    id: true,
-                    height: true,
-                    width: true
-                }
-            },
-            owner: {
-                select: {
-                    id: true,
-                    path: true,
-                    picture: true
-                }
-            }
-        }
-    })
-
-    return (
-        <UList
-            className="grid gap-8"
-            items={posts}
-            itemHandler={item => <Post {...item} />}
-        />
-    )
+    // return (
+    //     <UList
+    //         className="grid gap-8"
+    //         items={posts}
+    //         itemHandler={item => <Post {...item} />}
+    //     />
+    // )
     // console.log(posts[0])
     // const mediaHandler = media => <Post {...media} /> 
     // return <img src={`/api/media/${posts[0].picture}`} />      
-    // return (
-    //     <InfiniteList
-    //         className="grid gap-8"
-    //         path={path}
-    //         threshold={1000}
-    //         mediaHandler={mediaHandler}
-    //     />
-    // )
+    return (
+        <InfiniteList
+            className="grid gap-8"
+            path={path}
+            threshold={1000}
+            mediaHandler={mediaHandler}
+        />
+    )
 }
