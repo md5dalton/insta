48f20a752b0afb953074015fffefe7809e90f711
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-11-13 08:51:43 +0200

    Update Info.js

--- app/user/[username]/Info.js
+++ app/user/[username]/Info.js
@@ -1,38 +1,24 @@
-import Image from "next/image"
 import Link from "next/link"
-import Profile from "../../../components/Profile"
-import UList from "../../../components/UList"
-import { fetchAPI } from "../../utils"
+import Profile from "@/components/Profile"
+import UList from "@/components/UList"
 
-export default async () => {
-
-    const { data: { user }, error } = await fetchAPI("/user/dalii")
-
-    const {
-        name,
-        category,
-        description,
-        links
-    } = user
-
-    return (
-        <div className="container user-info">
-            <div className="profile-wrapper">
-                <Profile {...user} isLink={false} size="L" />
-            </div>
-            <div className="grid gap-[2px]">
-                <p className="font-bold">{name}</p>
-                <p className="text-white/50">{category}</p>
-                <p>{description}</p>
-                <UList
-                    items={links}
-                    itemHandler={({ text, url }) => <Link 
-                        prefetch={false} 
-                        href={url}
-                        className="font-semibold text-gray-500"
-                    >{text}</Link>}
-                />
-            </div>
+export default ({ id, name, picture, category, description, links }) => (
+    <div className="container user-info">
+        <div className="profile-wrapper">
+            <Profile {...{name, id, picture, isLink: false, size: "L"}}/>
         </div>
-    )
-}
+        <div className="grid gap-[2px]">
+            <p className="font-bold">{name}</p>
+            <p className="text-white/50">{category}</p>
+            <p>{description}</p>
+            <UList
+                items={links}
+                itemHandler={({ text, url }) => <Link 
+                    prefetch={false} 
+                    href={url}
+                    className="font-semibold text-gray-500"
+                >{text}</Link>}
+            />
+        </div>
+    </div>
+)
