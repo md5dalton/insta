f1eb3de7f286c7df794b437ac0d7dd11167491a1
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-11-13 08:51:29 +0200

    Update route.js

--- app/api/feed/[page]/route.js
+++ app/api/feed/[page]/route.js
@@ -3,11 +3,11 @@ import { getPosts } from "@/actions/feed"
 export async function GET(request, { params: { page } }) {
 
     const posts = await getPosts()
-
+    
     return new Response(JSON.stringify({
         media: posts,
         page,
-        end: false
+        end: posts.length == 0
 
     }), { status: 200 })
 
