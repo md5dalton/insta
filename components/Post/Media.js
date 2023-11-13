155a37725c7b5e2fd478e524de0620be080e83cb
md5dalton <md5daltonggs@gmail.com>
2023-11-13 23:36:42 +0200

    Create Media.js

---
+++ components/Post/Media.js
@@ -0,0 +1,20 @@
+import { useMediaCarousel } from "@/context/MediaCarousel"
+import { useInView } from "react-intersection-observer"
+import Image from "./Image"
+import Video from "./Video"
+
+export default props => {
+
+    const {setInView} = useMediaCarousel()
+
+    const [ref, inView] = useInView({
+        threshold: 1,
+        onChange: inView => inView ? setInView(props) : 0
+    })
+
+    return (
+        <div ref={ref}>
+            {props.video ?  <Video {...props} /> : <Image {...props} />}
+        </div>
+    )
+}
