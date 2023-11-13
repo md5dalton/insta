dd6a905568b73d482fc3c70561671c9b5f0a6fd2
md5dalton <md5daltonggs@gmail.com>
2023-11-13 23:36:46 +0200

    Update MediaCarousel.js

--- components/Post/MediaCarousel.js
+++ components/Post/MediaCarousel.js
@@ -1,14 +1,25 @@
+import { useMediaCarousel } from "@/context/MediaCarousel"
 import UList from "../UList"
-import Image from "./Image"
-import Video from "./Video"
+import Media from "./Media"
 
-export default ({ media }) => (
-    <UList
-        className="flex overflow-x-scroll snap-x snap-mandatory"
-        itemHandlerProps={{
-            className: "grid justify-center shrink-0 w-full snap-start snap-always"
-        }}
-        items={media}
-        itemHandler={media => media.video ? <Video {...media} /> : <Image {...media} />}
-    />
-)
+export default ({ media }) => {
+    
+    const { getCurrent } = useMediaCarousel()
+
+    return (
+        <div className="relative">
+            <div className="absolute bg-black/50 rounded-xl top-4 right-4 w-10 h-6 grid place-items-center text-xs font-bold">
+                <p>{getCurrent(media) || 1}/{media.length}</p>
+            </div>
+            <UList
+                className="flex overflow-x-scroll snap-x snap-mandatory"
+                itemHandlerProps={{
+                    className: "grid justify-center shrink-0 w-full snap-start snap-always"
+                }}
+                items={media}
+
+                itemHandler={media => <Media {...media} />}
+            />
+        </div>
+    )
+}
