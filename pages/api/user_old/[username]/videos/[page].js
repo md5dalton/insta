35de5e267264bbf309abcd36138af2a12e29a408
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-11-13 08:52:29 +0200

    Create [page].js

---
+++ pages/api/user_old/[username]/videos/[page].js
@@ -0,0 +1,26 @@
+import { shuffle } from "../../../../../utils/functions"
+
+const images = [
+  "/images/iggy/image-4.jpg",
+  "/images/iggy/image-1.jpg",
+  "/images/iggy/image-2.jpg",
+  "/images/iggy/image-3.jpg",
+]
+
+export default function handler(req, res) {
+  const { page } = req.query
+
+  const media = [1,2,3,4,5,6,7,8,9,0].map(i => ({
+    id: `page${page}:item${i}`,
+    thumb: shuffle(images).pop(),
+    stats: {
+      views: "482k"
+    }
+  }))
+
+  res.status(200).json({
+    media,
+    page,
+    end: false
+  })
+}
