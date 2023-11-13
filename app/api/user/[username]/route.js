f23c1d26924c770c1feb2629ce69735384e2936d
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-11-13 08:51:35 +0200

    Create route.js

---
+++ app/api/user/[username]/route.js
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server"
+
+export async function GET(request, { params: { username } }) {
+
+    const user = {
+        name: "Mpho Mothae",
+        hasNew: true,
+        username: username,
+        picture: "/images/iggy/profile.jpg",
+        category: "Personality",
+        description: "This is my official Letlapa account. Follow me and keep up to date of what I am up to.",
+        links: [
+            {
+                text: "whatsapp/me",
+                url: "https://whatsapp.me/234322"
+            },
+            {
+                text: "linktr.ee/iggy",
+                url: "https://linktr.ee/iggy"
+            },
+            {
+                text: "twitter/iggy",
+                url: "https://twitter.com/iggy"
+            },
+        ]
+    }
+  
+    return new NextResponse.json(user)
+
+}
