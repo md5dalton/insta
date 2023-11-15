8f36548a18915b5db09a6d65d6e624a61e174a09
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-11-15 13:52:35 +0200

    Update user.js

--- actions/user.js
+++ actions/user.js
@@ -1,10 +1,21 @@
 import prisma from "@/utils/prisma"
 
-export const getUser = async (user) => await prisma.user.findUnique({
+export const getUser = async user => await prisma.user.findUnique({
     where: {
         id: user
     },
     include: {
         owner: true
+    }
+})
+
+export const getUserPosts = async (user, page) => await prisma.post.findMany({
+    where: {
+        ownerId: user
+    },
+    skip: page * 10,
+    take: 10,
+    orderBy: {
+        path: "desc"
     }
 })
