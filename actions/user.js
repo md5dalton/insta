9041904f46a81be2d2513d326d94f6a163371fe1
md5dalton <md5daltonggs@gmail.com>
2023-12-14 20:46:45 +0200

    Update user.js

--- actions/user.js
+++ actions/user.js
@@ -9,7 +9,7 @@ export const getUser = async user => await prisma.user.findUnique({
     }
 })
 
-export const getUserPosts = async (user, page) => await prisma.post.findMany({
+export const getPosts = async (user, page) => await prisma.post.findMany({
     where: {
         ownerId: user
     },
@@ -18,4 +18,15 @@ export const getUserPosts = async (user, page) => await prisma.post.findMany({
     orderBy: {
         path: "desc"
     }
+})
+
+export const getReels = async (user, page) => await prisma.reel.findMany({
+    where: {
+        ownerId: user
+    },
+    skip: page * 10,
+    take: 10,
+    // orderBy: {
+    //     path: "desc"
+    // }
 })
