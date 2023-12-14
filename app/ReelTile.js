72e5987116b4888b888b6f43109121c2392533b5
md5dalton <md5daltonggs@gmail.com>
2023-12-14 20:47:00 +0200

    Update ReelTile.js

--- app/ReelTile.js
+++ app/ReelTile.js
@@ -9,27 +9,26 @@ const TileStat = ({ name, value }) => (
     </div>
 )
 
-export default ({ thumb, id, owner }) => {
-    
+export default ({ picture, id }) => {
+
     const width = "calc(100vw / 3)"
     const height = "calc(100vh / 3)"
 
     return (
-        <div className="reel-tile grid">
-            <Image
+        <div className="reel-tile grid bg-white/50" style={{height, width}}>
+            <img
                 className="row-span-full col-span-full"
-                src={thumb}
-                alt="reel tile"
+                src={picture}
+                // alt="reel tile"
                 width={100}
                 height={100}
                 style={{objectFit: "cover", height: height, width: width}}
             />
-            <Link href={`/reel/${id}${owner ? "?user=" + owner.username : ""}`} className="row-span-full col-span-full grow-0 bg-black/50 p-2 font-bold opacity-0 transition-all hover:opacity-100">
+            <Link href={`/reel/${id}`} className="row-span-full col-span-full grow-0 bg-black/50 p-2 font-bold opacity-0 transition-all hover:opacity-100">
                 <div className="grid h-full items-end">
                     <TileStat name="play-circle" value="596k" />
                 </div>
             </Link>
-            
         </div>
     )
 }
