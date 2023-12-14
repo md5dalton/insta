8c037927745308e66bf5289f48ca9a18e488e953
md5dalton <md5daltonggs@gmail.com>
2023-12-14 23:47:23 +0200

    Update route.js

--- app/api/user/[username]/reels/[page]/route.js
+++ app/api/user/[username]/reels/[page]/route.js
@@ -3,9 +3,9 @@ import { getReels } from "@/actions/user"
 export async function GET(req, { params: { username, page } }) {
     
     const media = await getReels(username, page)
-    
+    console.log(media)
     return Response.json({
-        media: media.map(({ id, picture }) => ({id, picture})),
+        media: media.map(({ id }) => ({id})),
         page,
         end: media.length <= 10
     })
