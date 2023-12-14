b4632bfdc6f9f69cc78eb078e0e6b8e8aea969f9
md5dalton <md5daltonggs@gmail.com>
2023-12-14 23:24:12 +0200

    Update Media.js

--- generator/Media.js
+++ generator/Media.js
@@ -4,19 +4,9 @@ export default class Media
 
     path
 
-    constructor (path, stats, metadata, isVideo) {
-        
-        const { birthtime } = stats
-        const { height, width } = metadata
+    constructor (path) {
         
         this.path = path
-
-        this.height = height
-        this.width = width
-        this.mktime = birthtime
-        this.portrait = height > width
-
-        this.isVideo = isVideo
 
     }
 
