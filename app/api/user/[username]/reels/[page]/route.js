38fc980fe01442f2c11581e282e8988d098d43ad
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-12-15 22:10:56 +0200

    Update route.js

--- app/api/user/[username]/reels/[page]/route.js
+++ app/api/user/[username]/reels/[page]/route.js
@@ -3,7 +3,7 @@ import { getReels } from "@/actions/user"
 export async function GET(req, { params: { username, page } }) {
     
     const media = await getReels(username, page)
-    console.log(media)
+    
     return Response.json({
         media: media.map(({ id }) => ({id})),
         page,
