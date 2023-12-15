b4c7ea498b2e8a1c8d6605ae0c15549faab7ac76
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-12-15 22:10:59 +0200

    Update Reel.js

--- app/reel/[id]/Reel.js
+++ app/reel/[id]/Reel.js
@@ -1,9 +1,9 @@
 import Player from "./Player"
 
-export default ({ height, id, owner}) => (
+export default ({ id, owner}) => (
     <div
-        style={{height: height}} 
-        className="reel grid snap-end snap-always">
+        // style={{height: height}} 
+        className="reel grid snap-end snap-always h-full grid-rows-1">
         <Player src={`/api/stream/${id}`} owner={owner} />
     </div>
 )
