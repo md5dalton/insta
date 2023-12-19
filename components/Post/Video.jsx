7ad44d60af13fae29eb34bcc0cc70ebd0b4daa3d
md5dalton <md5daltonggs@gmail.com>
2023-12-19 09:27:58 +0200

    Update Video.jsx

--- components/Post/Video.jsx
+++ components/Post/Video.jsx
@@ -1,15 +1,32 @@
-export default ({ reel: { id } }) => (
-   <div>
-        <video
-            poster={`/api/thumb/${id}`}
-            // autoPlay
-            muted
-            className="row-span-full col-span-full"
-            // src={url}
-        />
-        <div className="overlay row-span-full col-span-full flex flex-col">
-            <div className="controls grow"></div>
-            {/* <Indicators items={images} current={index} /> */}
-        </div>
-   </div>
-)
+import { setVideo } from "@/signals/video"
+import Link from "next/link"
+import { useRef } from "react"
+import { Waypoint } from "react-waypoint"
+
+export default ({ reel: { id } }) => {
+
+    const video = useRef()
+    
+    return (
+        <Waypoint
+            scrollableAncestor="window"
+            onEnter={()=>setVideo(video.current)}
+            onLeave={()=>console.log("leave", id)}
+        >
+            <Link href={`/reel/${id}`}>
+                <video
+                    ref={video}
+                    poster={`/api/thumb/${id}`}
+                    // autoPlay
+                    muted
+                    className="row-span-full col-span-full"
+                    src={`/api/stream/${id}`}
+                />
+                <div className="overlay row-span-full col-span-full flex flex-col">
+                    <div className="controls grow"></div>
+                    {/* <Indicators items={images} current={index} /> */}
+                </div>
+            </Link>
+        </Waypoint>
+    )
+}
