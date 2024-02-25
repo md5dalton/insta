dc0ccd33251b89a08f8437f57c952d8c868fa6a1
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-02-25 22:18:39 +0200

    Update route.js

--- app/api/feed/[page]/route.js
+++ app/api/feed/[page]/route.js
@@ -4,10 +4,10 @@ export async function GET(req, { params: { page } }) {
 
     const posts = await getPosts()
     
-    return new Response(JSON.stringify({
+    return Response.json({
         media: posts,
         page,
         end: posts.length == 0
-    }), { status: 200 })
+    })
 
 }
