2b81d29497a8e3951297df83c75e7618f4cf66bd
md5dalton <md5daltonggs@gmail.com>
2024-06-12 14:40:27 +0200

    Update route.js

--- app/api/reels/[id]/[page]/route.js
+++ app/api/reels/[id]/[page]/route.js
@@ -1,12 +1,13 @@
-import { getReel } from "@/actions/reel"
+import { getReel, getReels } from "@/actions/reel"
 
 export async function GET(request, { params: { id, page } }) {
     
     const reel = await getReel(id)
-    // console.log(reel)
+
+    const reels = await getReels(reel.owner.id)
 
     return Response.json({
-        media: [reel],
+        media: [reel, ...reels],
         page,
         end: true
     })
