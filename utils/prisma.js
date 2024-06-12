43517f54a175eb55c006b4862206b72cc0073431
md5dalton <md5daltonggs@gmail.com>
2024-06-12 15:03:58 +0200

    Update prisma.js

--- utils/prisma.js
+++ utils/prisma.js
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client"
+import prismaRandom from "prisma-extension-random"
 
-const prisma = globalThis.prisma || new PrismaClient()
+const prisma = globalThis.prisma || new PrismaClient().$extends(prismaRandom())
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma
 
