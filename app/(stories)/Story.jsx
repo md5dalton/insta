0fbfee33d06b26f2ae2aae4578229334b87a99f8
md5dalton <md5daltonggs@gmail.com>
2024-06-12 12:47:07 +0200

    Update Story.jsx

--- app/(stories)/Story.jsx
+++ app/(stories)/Story.jsx
@@ -1,14 +1,19 @@
-import ProfilePhoto from "@/components/elements/ProfilePhoto"
+import Image from "next/image"
+import Link from "next/link"
 
-export default ({ id, name, picture, hasNew }) => (
-    <div className="shrink-0">
-        <ProfilePhoto
-            url={`/story/${id}`}
-            src={`/api/media/${picture}`}
-            alt=""
-            size="M"
-            hasNew={hasNew}
-        />
-        <p style={{width: 56}} className="text-xs pt-1 truncate">{name}</p>
-    </div>
+export default ({ id, name, picture, hasNew = true }) => (
+    <Link href={`/stories/${id}`} className="shrink-0 grid w-24 gap-1">
+        <div className={`${hasNew ? "accent-ring " : ""}rounded-full p-1`}>
+            <div className="h-24 w-24 rounded-full border-4 border-black">
+                <Image
+                    height={96}
+                    width={96}
+                    src={`/api/media/${picture}`}
+                    alt=""
+                    className="object-cover rounded-full w-full h-full"
+                />
+            </div>
+        </div>
+        <p className="text-xs text-center truncate">long ass name {name}</p>
+    </Link>
 )
