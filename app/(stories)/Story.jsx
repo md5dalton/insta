6371cb8b4230fb68b638025d4a692c5edd2dec26
md5dalton <md5daltonggs@gmail.com>
2024-06-12 18:19:46 +0200

    Update Story.jsx

--- app/(stories)/Story.jsx
+++ app/(stories)/Story.jsx
@@ -3,9 +3,9 @@ import Image from "next/image"
 import Link from "next/link"
 
 export default ({ id, name, picture, hasNew = true }) => (
-    <Link href={`/stories/${id}`} className="shrink-0 grid w-24 gap-1">
-        <div className={`${hasNew ? "accent-ring " : ""}rounded-full p-1`}>
-            <div className="h-24 w-24 rounded-full border-4 border-black">
+    <Link href={`/stories/${id}`} className="shrink-0 grid w-20 gap-1">
+        <div className={`${hasNew ? "accent-ring " : ""}rounded-full p-[3px]`}>
+            <div className="h-20 w-20 rounded-full border-4 border-black">
                 <Image
                     height={96}
                     width={96}
@@ -15,6 +15,6 @@ export default ({ id, name, picture, hasNew = true }) => (
                 />
             </div>
         </div>
-        <p className="text-xs text-center truncate">long ass name {name}</p>
+        <p className="text-xs text-center truncate">{name}</p>
     </Link>
 )
