ad7c11d1ca4574d32e977a714c89feccba1bc3ae
md5dalton <md5daltonggs@gmail.com>
2024-06-12 19:37:01 +0200

    Update Feed.jsx

--- app/(feed)/Feed.jsx
+++ app/(feed)/Feed.jsx
@@ -1,33 +1,33 @@
-import { getPosts } from "@/actions/feed"
-import LoadMore from "./LoadMore"
+// import { getPosts } from "@/actions/feed"
+// import LoadMore from "./LoadMore"
 
-export default async () => {
+// export default async () => {
     
-    const data = await getPosts()
+//     const data = await getPosts()
 
-    return (
-        <ul className="grid gap-8">
-            {data}
-            <LoadMore />
-        </ul>
-
-    )
-}
-
-// "use client"
-// import Post from "@/components/Post/Post"
-// import InfiniteList from "../InfiniteList"
-
-// export default ({ path }) => {
-
-//     const mediaHandler = media => <Post {...media} /> 
-   
 //     return (
-//         <InfiniteList
-//             className=""
-//             path={path}
-//             threshold={1000}
-//             mediaHandler={mediaHandler}
-//         />
+//         <ul className="grid gap-8">
+//             {data}
+//             <LoadMore />
+//         </ul>
+
 //     )
 // }
+
+"use client"
+import Post from "@/components/Post/Post"
+import InfiniteList from "../InfiniteList"
+
+export default () => {
+
+    const mediaHandler = media => <Post {...media} /> 
+
+    return (
+        <InfiniteList
+            className=""
+            path={"/feed"}
+            threshold={1000}
+            mediaHandler={mediaHandler}
+        />
+    )
+}
