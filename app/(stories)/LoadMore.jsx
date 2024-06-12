096088f0441b4d68f95367b672600cd4652b322b
md5dalton <md5daltonggs@gmail.com>
2024-06-12 18:19:53 +0200

    Update LoadMore.jsx

--- app/(stories)/LoadMore.jsx
+++ app/(stories)/LoadMore.jsx
@@ -1,36 +1,4 @@
-"use client"
-
 import { getStories } from "@/actions/stories"
-import { useInView } from "react-intersection-observer"
-import { useEffect, useState } from "react"
+import LoadMore from "@/components/LoadMore"
 
-let page = 2
-
-export default () => {
-    
-    const { ref, inView } = useInView()
-
-    const [ data, setData ] = useState([])
-    
-    
-    useEffect(() => {
-        if (inView) {
-            
-            getStories(page).then(res => {
-                setData([...data, ...res])
-                page++
-            })
-
-        }
-    }, [inView, data])
-
-    
-
-    return (
-        <>
-            {data}
-            <div ref={ref} />
-        </>
-
-    )
-}
+export default () => <LoadMore handler={getStories} />
