7abb5b04e1acf56040f42f4e68710211db21e2f4
md5dalton <md5daltonggs@gmail.com>
2024-06-12 18:19:50 +0200

    Create LoadMore.jsx

---
+++ components/LoadMore.jsx
@@ -0,0 +1,34 @@
+"use client"
+import { useInView } from "react-intersection-observer"
+import { useEffect, useState } from "react"
+
+let page = 2
+
+export default ({ handler }) => {
+    
+    const { ref, inView } = useInView()
+
+    const [ data, setData ] = useState([])
+    
+    
+    useEffect(() => {
+        if (inView) {
+            
+            handler(page).then(res => {
+                setData([...data, ...res])
+                page++
+            })
+
+        }
+    }, [inView, data])
+
+    
+
+    return (
+        <>
+            {data}
+            <div ref={ref} />
+        </>
+
+    )
+}
