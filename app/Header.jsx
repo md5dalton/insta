fbcf92a6297152aacf1f3b21fe7c541e287b0f46
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-07-26 15:54:01 +0200

    Update Header.jsx

--- app/Header.jsx
+++ app/Header.jsx
@@ -1,16 +1,17 @@
 import Link from "next/link"
-import Icon from "../components/elements/Icon"
-
-const LinkIcon = ({ href, icon }) => (
-    <Link href={href} className="pt-3 pb-4 grid place-items-center">
-        <Icon name={icon} />
-    </Link> 
-)
+import { Bars3Icon, UserIcon } from "@heroicons/react/24/outline"
+import IconButton from "@/components/IconButton"
 
 export default () => (
-    <header className="sticky top-0 z-50 container bg-black flex items-center">
-        <LinkIcon icon="focus" href="/create" />
-        <Link className="grow text-center font-bold tracking-wider" href="/">LETLAPA</Link>
-        <LinkIcon icon="paper-plane" href="/share" />
+    <header className="sticky top-0 z-50 container bg-very-dark-grey flex items-center py-1">
+        <Link className="text-lg font-bold tracking-wider" href="/">LETLAPA</Link>
+        <div className="grow flex gap-6 justify-end">
+            <IconButton>
+                <UserIcon height={24} />
+            </IconButton>
+            <IconButton>
+                <Bars3Icon height={24} />
+            </IconButton>
+        </div>
     </header>
 )
