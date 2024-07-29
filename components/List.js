c1945d1329351b6793289fbd238f74180906eb24
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-07-29 18:07:53 +0200

    Delete List.js

--- components/List.js
+++
@@ -1,8 +0,0 @@
-export default ({ items, itemHandler, empty, ...props }) => (
-    <ul {...props} role="list">
-        {
-            !items.length ? empty :
-            items.map(itemHandler)
-        }
-    </ul>
-)
