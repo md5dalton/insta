c1e069204e0da802a6765cdcc75ea68c29803b7f
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-07-29 21:20:20 +0200

    Update route.js

--- app/api/explore/[page]/route.js
+++ app/api/explore/[page]/route.js
@@ -1,25 +1,33 @@
 import { getPosts } from "@/actions/feed"
+import { find } from "@/generator/finder"
+import { shuffle } from "@/utils/functions"
 
 export async function GET(req, { params: { page } }) {
 
     // const posts = await getPosts()
+    const thumbs = await find("/media/_thumbs", ["jpg"])
 
-    const post = {picture: "t:sfse", hasReel: true}
+    const ids = shuffle(thumbs.map(i => i.split("/").pop().split(".").shift()))
+
+    // console.log(ids)
+    const post = {hasReel: true, p: page}
 
     const posts =[]
 
     const media = []
 
-    for (let index = 0; index < 8; index++) posts.push({id: index, ...post})
+    const pageSize = 8
+
+    for (let index = 0; index < pageSize; index++) posts.push({id: index, picture: `t:${ids[page*pageSize + index]}`, ...post})
 
     for (let i = 0; i < posts.length; i += 2) media.push(posts.slice(i, i + 2))
     
     media.splice(2, 0, [{id: 0, reel: true}], [{id: 1, reel: true}])
-    
+    console.log(media.length)
     return Response.json({
         media: media,
         page,
-        end: true
+        end: false
     })
 
 }
