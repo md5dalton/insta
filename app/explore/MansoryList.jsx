a21e009d58724d55f7b76f9e2073d6e2e4297d24
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-07-29 18:08:02 +0200

    Update MansoryList.jsx

--- app/explore/MansoryList.jsx
+++ app/explore/MansoryList.jsx
@@ -1,27 +1,43 @@
 "use client"
 import InfiniteScroll from "react-infinite-scroller"
-import useMedia from "../../../hooks/useMedia"
 import MansoryColumn from "./MansoryColumn"
 import UList from "@/components/UList"
+import useMedia from "@/hooks/useMedia"
+import List from "@/components/List"
 
 export default () => {
 
-    const { 
-        media, error, isLoading, end,
-        loadmore
-    } = useMedia("/explore")
+    // const { 
+    //     media, error, isLoading, end,
+    //     loadmore
+    // } = useMedia("/explore")
     
-    const loadmoreHandler = page => !end && !isLoading ? loadmore() : 0
+    // const loadmoreHandler = page => !end && !isLoading ? loadmore() : 0
+    
+    const post = {picture: "t:sfse", hasMany: true}
 
+    const posts =[]
+
+    const media = []
+
+    for (let index = 0; index < 8; index++) posts.push({id: index, ...post})
+
+    for (let i = 0; i < posts.length; i += 2) media.push(posts.slice(i, i + 2))
+    
+    media.splice(2, 0, [{id: 0, reel: true}], [{id: 1, reel: true}])
+    // console.log(media)
     return (
         <InfiniteScroll
-            loadMore={loadmoreHandler}
-            hasMore={!end}
-            className="grid grid-cols-3 gap-[3px]"
-            threshold={600}
-            element="ul"
+            loadMore={()=>1}
+            hasMore={true}
+            // threshold={600}
+            // element="ul"
         >
             <UList
+                className="grid grid-cols-3 gap-[3px]"
+                itemHandlerProps={{
+                    className: "grid"
+                }}
                 items={media}
                 itemHandler={item => <MansoryColumn items={item} />}
             />
