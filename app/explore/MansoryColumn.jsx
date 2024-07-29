1d78f9707e436d26e657af8590904cca1f380f5d
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-07-29 21:20:17 +0200

    Update MansoryColumn.jsx

--- app/explore/MansoryColumn.jsx
+++ app/explore/MansoryColumn.jsx
@@ -2,11 +2,13 @@ import List from "@/components/List"
 import ReelTile from "./ReelTile"
 import PostTile from "./PostTile"
 
-export default  ({ items }) => (
+export default  ({ items }) => {
+    // console.log(items)  
+    return (
     <List
         style={{height: "calc(200vw / 3)"}}
         className="grid gap-[3px] grid-rows-2"
         items={items}
         itemHandler={item => item.reel ? <ReelTile key={item.id} {...item} /> : <PostTile key={item.id} {...item} /> }
     />
-)
+)}
