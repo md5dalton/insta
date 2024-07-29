0269ac951e17a6564e0a84aeb922a6262eab3486
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-07-29 21:20:10 +0200

    Update ReelTile.jsx

--- app/explore/ReelTile.jsx
+++ app/explore/ReelTile.jsx
@@ -5,7 +5,7 @@ export default ({ id }) =>  (
     <Link href={`/reel/${id}`} className="grid bg-white/50 row-span-2">
         <Image
             className="row-span-full col-span-full h-full w-full object-cover"
-            src={`/api/thumb/5So3sLIu`}
+            src={`/api/thumb/7gPfYJ0I`}
             alt="reel tile"
             width={100}
             height={100}
