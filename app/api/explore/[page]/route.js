24e8842f6d22f472807cb585d3a5a5f85cf6fe42
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-07-29 18:07:32 +0200

    Update route.js

--- app/api/explore/[page]/route.js
+++ app/api/explore/[page]/route.js
@@ -4,18 +4,22 @@ export async function GET(req, { params: { page } }) {
 
     // const posts = await getPosts()
 
-    const post = {picture: "sfse", id: "sdv"}
-    const reel = {id: "sdv"}
+    const post = {picture: "t:sfse", hasReel: true}
 
     const posts =[]
-    const reels =[]
 
-    for (let index = 0; index < 12; index++) posts.push(post)
+    const media = []
+
+    for (let index = 0; index < 8; index++) posts.push({id: index, ...post})
+
+    for (let i = 0; i < posts.length; i += 2) media.push(posts.slice(i, i + 2))
+    
+    media.splice(2, 0, [{id: 0, reel: true}], [{id: 1, reel: true}])
     
     return Response.json({
-        media: posts,
+        media: media,
         page,
-        end: posts.length == 0
+        end: true
     })
 
 }
