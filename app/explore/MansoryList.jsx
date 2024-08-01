7bbdc2f1e1fc7ce72a28f3a621d5932fa49a78c7
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-08-01 16:15:05 +0200

    Update MansoryList.jsx

--- app/explore/MansoryList.jsx
+++ app/explore/MansoryList.jsx
@@ -1,9 +1,7 @@
 "use client"
-import InfiniteScroll from "react-infinite-scroller"
 import MansoryColumn from "./MansoryColumn"
 import UList from "@/components/UList"
 import useMedia from "@/hooks/useMedia"
-import List from "@/components/List"
 import { useInView } from "react-intersection-observer"
 import { useEffect } from "react"
 import { explore } from "@/signals/sizes"
@@ -13,39 +11,28 @@ export default () => {
     const { ref, inView } = useInView()
 
     const { 
-        media, error, isLoading, end, size,
+        media, size,
         loadmore
     } = useMedia("/explore", explore.value)
     
     useEffect(() => {
-        if (inView) {
-            console.log("in view")
-            loadmore()
-        }
+        if (inView) loadmore()
     }, [inView])
+
     useEffect(() => {
-        console.log("explore.value", explore.value)
-        console.log("size", size)
         if (size > explore.value) explore.value = size + 1
     }, [size])
-    console.log(explore.value)
+    
     return (
-        // <InfiniteScroll
-        //     loadMore={loadmore}
-        //     hasMore={true}
-        //     threshold={600}
-        //     element="ul"
-        // >
-            <UList
-                className="grid grid-cols-3 gap-[3px]"
-                itemHandlerProps={{
-                    className: "grid"
-                }}
-                items={media}
-                itemHandler={item => <MansoryColumn items={item} />}
-            >
-                <div ref={ref} />
-            </UList>
-        // </InfiniteScroll>
+        <UList
+            className="grid grid-cols-3 gap-[3px]"
+            itemHandlerProps={{
+                className: "grid"
+            }}
+            items={media}
+            itemHandler={item => <MansoryColumn items={item} />}
+        >
+            <div ref={ref} />
+        </UList>
     )
 }
