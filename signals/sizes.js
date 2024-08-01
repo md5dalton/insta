2be1c686559427bf79b9000390873a024a898188
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-08-01 16:14:58 +0200

    Update sizes.js

--- signals/sizes.js
+++ signals/sizes.js
@@ -1,15 +1,3 @@
 import { signal } from "@preact/signals-react"
 
 export const explore = signal(2)
-
-// console.log(video.value)
-
-export const setVideo = vid => {
-    
-    if (video.value && !video.value?.paused) video.value.pause()
-    
-    // console.log(video.value)
-    video.value = vid
-    video.value.play()
-
-}
