c57e703c800557b71e39250f127130ff4715ba2c
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-08-01 16:05:37 +0200

    Update useMedia.js

--- hooks/useMedia.js
+++ hooks/useMedia.js
@@ -1,5 +1,4 @@
-import { API } from "../utils/constants"
-import { fetcher } from "../utils/functions"
+import { fetcher } from "@/utils/functions"
 import useSWRInfinite from "swr/infinite"
 
 export default (path, initialSize) => {
@@ -11,27 +10,21 @@ export default (path, initialSize) => {
         setSize, mutate  
     } = useSWRInfinite(index => url + index, fetcher, {initialSize})
 
+    const isLoadingInitialData = !data && !error
+
     let end = true
     
     let media = []
     
     if (data) {
         for (const item of data) for (const i of item.media) media.push(i) 
-            // console.log([...item.media])
-            // media = [...item.media]
-        // }
         end = data[size - 1]?.end
-        // console.log(data, size)
     }
+
     return {
         media,
         size,
         error,
-        loadmore: () => !isLoading && !error ? setSize(size + 1) : null
+        loadmore: () => !isLoading && !error && !isLoadingInitialData ? setSize(size + 1) : null
     }
 }
-
-    // const isLoadingInitialData = !data && !error
-    // const isLoadingMore =
-    // isLoadingInitialData ||
-    // (size > 0 && data && typeof data[size - 1] === "undefined")
