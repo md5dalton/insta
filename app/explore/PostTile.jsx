06e188a7c0b51b25c0623493f56c3753d1b75a37
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-08-03 09:52:36 +0200

    Update PostTile.jsx

--- app/explore/PostTile.jsx
+++ app/explore/PostTile.jsx
@@ -3,15 +3,15 @@ import Link from "next/link"
 import { getImageUrl } from "@/utils/functions"
 import { RectangleStackIcon, VideoCameraIcon } from "@heroicons/react/24/solid"
 
-export default ({ picture, id, hasReel, hasMany }) =>  (
+export default ({ thumb, id, hasReel, hasMany }) =>  (
     <Link
         href={`/post/${id}`}
         className="grid bg-white/50 row-span-1 overflow-hidden"
     >
         <Image
             className="row-span-full col-span-full h-full w-full object-cover object-top"
-            src={getImageUrl(picture)}
-            alt="reel tile"
+            src={getImageUrl(thumb)}
+            alt=""
             width={100}
             height={100}
         />
