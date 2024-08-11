015889ca4d0d97e0480200320f0fb5a8a849b51b
Mpiti <md5daltonggs@gmail.com>
2024-08-11 09:55:28 +0200

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,12 @@
-import Feed from "./(feed)/Feed"
+// import Feed from "./(feed)/Feed"
 import Header from "./Header"
-import Stories from "./(stories)/Stories"
+// import Stories from "./(stories)/Stories"
 
 export default () => (
     <div className="home">
         <Header />
         <div className="py-3 line-b">
-            <Stories />
+            {/* <Stories /> */}
         </div>
         {/* <Feed path="/feed" /> */}
     </div>
