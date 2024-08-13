e6008e84a9fba14d57d220fbfd9a10abed93f107
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-08-13 15:00:50 +0200

    Delete prisma.js

--- utils/prisma.js
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client"
-import prismaRandom from "prisma-extension-random"
-
-const prisma = globalThis.prisma || new PrismaClient().$extends(prismaRandom())
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma
-
-export default prisma
