354f1809a43cdbb81b0d9f9387b061f4bed37c20
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2025-01-06 15:50:15 +0200

    Update store.js

--- store/store.js
+++ store/store.js
@@ -1,13 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { combineReducers, configureStore } from "@reduxjs/toolkit"
 import { feedReducer } from "./feed/feedSlice"
 import { instaApi } from "@/services/api"
 import { setupListeners } from "@reduxjs/toolkit/query"
+import storage from "redux-persist/lib/storage"
+import { persistReducer, persistStore } from "redux-persist"
+import { paginationReducer } from "./pagination/paginationSlice"
+
+const rootReducer = combineReducers({
+    [instaApi.reducerPath]: instaApi.reducer,
+    pagination: paginationReducer,
+})
+
+const persistConfig = {
+    key: "root",
+    storage,
+    whitelist: ["pagination"]
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const makeStore = () => configureStore({
-    reducer: {
-        [instaApi.reducerPath]: instaApi.reducer
-    },
-    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(instaApi.middleware)
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware({ serializableCheck: false }).concat(instaApi.middleware)
 })
+
+export const persistor = persistStore(makeStore())
 
 setupListeners(makeStore().dispatch)
