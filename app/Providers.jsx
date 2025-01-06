4cf89a8b099e21fead84443aaf629fdad591ed12
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2025-01-06 15:49:47 +0200

    Update Providers.jsx

--- app/Providers.jsx
+++ app/Providers.jsx
@@ -1,12 +1,16 @@
 "use client"
+import { PersistGate } from "redux-persist/integration/react"
 import Footer from "./Footer"
 import { StoreProvider } from "@/context/StoreProvider"
+import { persistor } from "@/store/store"
 
 export const Providers = ({ children }) => (
     <StoreProvider>
-        <main>
-            {children}
-        </main>
-        <Footer />
+        <PersistGate loading={null} persistor={persistor}>
+            <main>
+                {children}
+            </main>
+            <Footer />
+        </PersistGate>
     </StoreProvider>
 )
