0703dee4abe72544cabba7619fcd8d40461b8ab2
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2025-01-13 15:53:57 +0200

    Update Providers.jsx

--- app/Providers.jsx
+++ app/Providers.jsx
@@ -3,14 +3,17 @@ import { PersistGate } from "redux-persist/integration/react"
 import Footer from "./Footer"
 import { StoreProvider } from "@/context/StoreProvider"
 import { persistor } from "@/store/store"
+import { SessionProvider } from "next-auth/react"
 
 export const Providers = ({ children }) => (
-    <StoreProvider>
-        <PersistGate loading={null} persistor={persistor}>
-            <main>
-                {children}
-            </main>
-            <Footer />
-        </PersistGate>
-    </StoreProvider>
+    <SessionProvider>
+        <StoreProvider>
+            <PersistGate loading={null} persistor={persistor}>
+                <main>
+                    {children}
+                </main>
+                <Footer />
+            </PersistGate>
+        </StoreProvider>
+    </SessionProvider>
 )
