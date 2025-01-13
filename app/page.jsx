1883c3df6b3c887bedcd7d97a5c30af161258484
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2025-01-13 15:54:12 +0200

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,20 @@
+"use client"
+import Button from "@/components/elements/Button"
 import Feed from "./(feed)/Feed"
 import Header from "./Header"
+import { signIn } from "next-auth/react"
 // import Stories from "./(stories)/Stories"
 
 export default () => (
     <div className="home">
         <Header />
+        <Button
+            onClick={() => signIn("google")}
+            className="border-[1px] py-2 px-4 mx-auto my-4"
+        >login with google</Button>
         {/* <div className="py-3 line-b"> */}
             {/* <Stories /> */}
         {/* </div> */}
-        <Feed path="/feed" />
+        {/* <Feed path="/feed" /> */}
     </div>
 )
