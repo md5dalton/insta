aa9ae50f4902b677051581eb9ecc8ee42464d2fb
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2025-03-12 20:22:06 +0200

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,31 @@
+import { getToken } from "next-auth/jwt"
+import { NextResponse } from "next/server"
+
+export async function middleware(req) {
+
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+
+    const loginPath = "/login"
+
+    const { pathname } = req.nextUrl
+
+    if (pathname.startsWith(loginPath) || pathname.startsWith("/_next")) return NextResponse.next()
+    
+    if (!token) {
+
+        const loginUrl = new URL(loginPath, req.url)
+        
+        loginUrl.searchParams.set("callbackUrl", req.url)
+        
+        return NextResponse.redirect(loginUrl)
+
+    }
+
+    return NextResponse.next()
+
+}
+
+// Specify the paths where middleware should run
+export const config = {
+    matcher: ["/:path*"]
+}
