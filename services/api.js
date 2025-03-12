e7fcdd0b75ce440814c74dff11e6f19d2d063811
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2025-03-12 20:22:02 +0200

    Update api.js

--- services/api.js
+++ services/api.js
@@ -1,9 +1,25 @@
 "use client"
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { getSession } from "next-auth/react"
+
+const baseQuery = fetchBaseQuery({
+    
+    baseUrl: process.env.NEXT_PUBLIC_API,
+
+    prepareHeaders: async headers => {
+
+        const session = await getSession()
+      
+        if (session && session.token) headers.set('Authorization', `Bearer ${session.token}`)
+      
+        return headers
+    
+    }
+})
 
 export const instaApi = createApi({
     reducerPath: "instaApi",
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API }),
+    baseQuery,
     endpoints: builder => ({
         listFeedPosts: builder.query({
             query: (page = 1) => `/feed/${page}`,
